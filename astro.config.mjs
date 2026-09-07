3117ca9a746f2a519983d313d4bfc9c1db9bff32
Rafael Farias <rafael@rfarias.dev>
2026-09-07 22:00:18 +0000

    fix: atualizar domínio para rfarias.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://rfarias.dev',
+  site: 'https://rfarias.com',
   output: 'static',
   integrations: [sitemap()],
   markdown: {
